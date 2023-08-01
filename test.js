816fd2bd5045597361b840080deabf92cdd277cc
KajiAime <ndekaji@gmail.com>
2023-08-01 23:43:08 +0100

    test.js had to be removed.

--- test.js
+++
@@ -1,5 +0,0 @@
-var url = require('url');
-const link = "https://localhost:3000/api/2015-10-12";
-var q = url.parse(link, true);
-console.log(q.pathname);
-console.log(/[0-9]{4}-[0-9]{2}-[0-9]{2}$\b/.test(q.pathname));
